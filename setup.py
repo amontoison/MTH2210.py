79fa91424409da76b832ec028ae55e246b8f5980
Alexis Montoison <alexis.montoison@polymtl.ca>
2020-09-08 15:27:02 -0400

    patrick

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(name='MTH2210',
     description='Codes Python pour le cours MTH2210 de Polytechnique Montréal.',
     author='Pierre-Yves Bouchet',
     url='https://github.com/amontoison/MTH2210.py',
-    packages=['.'],
+    packages=['MTH2210'],
     install_requires=['numpy']
  )
