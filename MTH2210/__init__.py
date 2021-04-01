4dba14c7747260ce6c733fe28c473644888f026c
PierreYvesBouchet <pybouchet@gmail.com>
2021-04-01 18:38:28 -0400

    MàJ init.py pour ajout newton_nd_avec_der

--- MTH2210/__init__.py
+++ MTH2210/__init__.py
@@ -1,9 +1,9 @@
-from MTH2210.Racines_points_fixes.bissection import bissection
-from MTH2210.Racines_points_fixes.newton_1d  import newton_1d
-from MTH2210.Racines_points_fixes.newton_nd  import newton_nd
-from MTH2210.Racines_points_fixes.newton_nd  import newton_nd_avec_der
-from MTH2210.Racines_points_fixes.point_fixe import point_fixe
-from MTH2210.Racines_points_fixes.secante    import secante
+from MTH2210.Racines_points_fixes.bissection          import bissection
+from MTH2210.Racines_points_fixes.newton_1d           import newton_1d
+from MTH2210.Racines_points_fixes.newton_nd           import newton_nd
+from MTH2210.Racines_points_fixes.newton_nd_avec_der  import newton_nd_avec_der
+from MTH2210.Racines_points_fixes.point_fixe          import point_fixe
+from MTH2210.Racines_points_fixes.secante             import secante
 
 from MTH2210.Interpolations.lagrange   import lagrange
 from MTH2210.Interpolations.spline_cub import spline_cub
